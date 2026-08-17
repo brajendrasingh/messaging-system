7a18f5431405f8b75e1f7f49010480e820be8d17
brajendra <brajendrasingh3clogic@gmail.com>
2026-08-17 11:51:43 +0530

    SensorDataAggregate model added

--- kafka/kafka-streams/kafka-stream/src/main/java/com/bksoft/kafka_stream/model/SensorDataAggregate.java
+++ kafka/kafka-streams/kafka-stream/src/main/java/com/bksoft/kafka_stream/model/SensorDataAggregate.java
@@ -1,4 +1,70 @@
 package com.bksoft.kafka_stream.model;
 
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SensorDataAggregate {
+
+    private String sensorId;
+
+    private List<Temperature> temperatures = new ArrayList<>();
+
+    private Double humiditySum = 0.0;
+
+    private int count = 0;
+
+    private Instant firstTimestamp;
+
+    private Instant lastTimestamp;
+
+    public void add(SensorData sensor) {
+
+        this.sensorId = sensor.getSensorId();
+
+        if (sensor.getTemperatures() != null) {
+            temperatures.addAll(sensor.getTemperatures());
+        }
+
+        if (sensor.getHumidity() != null) {
+            humiditySum += sensor.getHumidity();
+        }
+
+        count++;
+
+        if (firstTimestamp == null ||
+                sensor.getTimestamp().isBefore(firstTimestamp)) {
+            firstTimestamp = sensor.getTimestamp();
+        }
+
+        if (lastTimestamp == null ||
+                sensor.getTimestamp().isAfter(lastTimestamp)) {
+            lastTimestamp = sensor.getTimestamp();
+        }
+    }
+
+    public boolean hasHighTemperature() {
+
+        return temperatures.stream()
+                .anyMatch(t ->
+                        t.getValue() != null &&
+                                t.getValue() > 1.0
+                );
+    }
+
+    public SensorData toSensorData() {
+
+        SensorData sensorData = new SensorData();
+
+        sensorData.setSensorId(sensorId);
+        sensorData.setTemperatures(temperatures);
+
+        if (count > 0) {
+            sensorData.setHumidity(humiditySum / count);
+        }
+
+        sensorData.setTimestamp(lastTimestamp);
+
+        return sensorData;
+    }
 }
