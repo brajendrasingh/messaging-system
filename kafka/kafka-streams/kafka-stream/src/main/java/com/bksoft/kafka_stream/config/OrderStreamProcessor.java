86afd319dc9ac25591578ebc985e2557cd0be800
brajendra <brajendrasingh3clogic@gmail.com>
2026-08-17 15:14:00 +0530

    refactoring

--- kafka/kafka-streams/kafka-stream/src/main/java/com/bksoft/kafka_stream/config/OrderStreamProcessor.java
+++ kafka/kafka-streams/kafka-stream/src/main/java/com/bksoft/kafka_stream/config/OrderStreamProcessor.java
@@ -1,13 +1,17 @@
 package com.bksoft.kafka_stream.config;
 
 import com.bksoft.kafka_stream.model.Order;
+import com.bksoft.kafka_stream.model.OrderAverage;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.serializer.JacksonJsonSerde;
 
 import java.math.BigDecimal;
+import java.time.Duration;
 
 @Configuration
 public class OrderStreamProcessor {
@@ -32,5 +36,31 @@ public class OrderStreamProcessor {
         .to(highValueOrderTopic);
 
         return orders;
+    }
+
+    @Bean
+    public KTable<Windowed<String>, Double> ordersAvgPrice(StreamsBuilder builder) {
+        KStream<String, Order> orders = builder.stream(orderTopic);
+
+        KTable<Windowed<String>, Double> result = orders.filter((key, order) -> order != null && order.getAmount() != null)
+                // Put all orders into one group
+                .selectKey((key, order) -> "ALL")
+                // Group orders
+                .groupByKey(Grouped.with(Serdes.String(), new JacksonJsonSerde<>(Order.class)))
+                // 5-minute tumbling window
+                .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofMinutes(5)))
+                .aggregate(OrderAverage::new, (key, order, average) -> {
+                            average.sum += order.getAmount().doubleValue();
+                            average.count++;
+                            return average;
+                        }, Materialized.as("order-average-store")
+                ).mapValues(OrderAverage::getAverage); // Convert OrderAverage to average Double
+
+        // Print the result whenever the KTable is updated
+        result.toStream()
+                .peek((windowedKey, average) -> System.out.println("Window: " + windowedKey.window().startTime() + " - " + windowedKey.window().endTime() + " | Average Order Amount = " + average))
+                .to("average-order-price");
+
+        return result;
     }
 }
---
+++ kafka/kafka-streams/kafka-stream/src/main/java/com/bksoft/kafka_stream/model/OrderAverage.java
@@ -0,0 +1,37 @@
+package com.bksoft.kafka_stream.model;
+
+public class OrderAverage {
+    public double sum;
+    public long count;
+
+    public OrderAverage() {
+    }
+
+    public OrderAverage(double sum, long count) {
+        this.sum = sum;
+        this.count = count;
+    }
+
+    public double getAverage() {
+        if (count == 0) {
+            return 0.0;
+        }
+        return sum / count;
+    }
+
+    public double getSum() {
+        return sum;
+    }
+
+    public void setSum(double sum) {
+        this.sum = sum;
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public void setCount(long count) {
+        this.count = count;
+    }
+}
